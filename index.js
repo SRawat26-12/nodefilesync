7d709648286b20a6e53c7cbb54eabccafa533e1f
shivam <shivamrawat1053@gmail.com>
2024-10-12 16:44:16 +0530

    filesystem

---
+++ index.js
@@ -0,0 +1,5 @@
+const fs=require("fs");
+//fs.mkdirSync("shivam");//create a new folder
+//fs.writeFileSync("shivam/bio.txt","my name is shivam");//create a file inside shivam folder and write data
+//fs.appendFileSync("shivam/bio.txt","my name is shivam rawat:)")
+
